2e724d6659ff4344a3ec92443bcfa9f93e4d1120
sun-shin <sunhanshin2@EMAIL.com>
2020-09-09 12:42:33 -0500

    display JSON for individual products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,23 @@
 class Api::ProductsController < ApplicationController
+
   def all_products_action
     @all_products = Product.all
     render "all_products.json.jb"
   end
+
+  def camera_action
+    @camera = Product.find_by(id:3)
+    render "camera.json.jb"
+  end
+
+  def fan_action
+    @fan = Product.find_by(id:2)
+    render "fan.json.jb"
+  end
+
+  def laptop_action
+    @laptop = Product.find_by(id:1)
+    render "laptop.json.jb"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     get "/all_products_path" => "products#all_products_action"
+    get "/camera_path" => "products#camera_action"
+    get "/fan_path" => "products#fan_action"
+    get "/laptop_path" => "products#laptop_action"
   end
 end
