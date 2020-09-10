67bd9fe56a725edcedf3cec255ea9d1c20c4b901
sun-shin <sunhanshin2@EMAIL.com>
2020-09-10 10:26:44 -0500

    made corrections to laptop.json.jb and added to front_end.rb to display all products

--- front_end.rb
+++ front_end.rb
@@ -1,5 +1,9 @@
 require "http"
 
-response = HTTP.get("http://localhost:3000/api/camera_path")
-p camera = response.parse
+response = HTTP.get("http://localhost:3000/api/all_products_path")
+pp all_products = response.parse
 
+require "http"
+
+# response = HTTP.get("http://localhost:3000/api/camera_path")
+# pp camera = response.parse
