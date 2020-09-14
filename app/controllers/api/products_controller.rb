a7ecb991de194865416c71d23c60022a6d5af142
sun-shin <sunhanshin2@EMAIL.com>
2020-09-14 13:17:41 -0500

    update and destroy rest methods

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,30 +1,68 @@
 class Api::ProductsController < ApplicationController
 
-  def all_products_action
-    @all_products = Product.all
-    render "all_products.json.jb"
+  # def all_products_action
+  #   @all_products = Product.all
+  #   render "all_products.json.jb"
+  # end
+
+  # def camera_action
+  #   @camera = Product.find_by(id:3)
+  #   render "camera.json.jb"
+  # end
+
+  # def fan_action
+  #   @fan = Product.find_by(id:2)
+  #   render "fan.json.jb"
+  # end
+
+  # def laptop_action
+  #   @laptop = Product.find_by(id:1)
+  #   render "laptop.json.jb"
+  # end
+
+  # def single_product_action
+  #   product_id = params[:id]
+  #   @message = Product.find_by(id: product_id)
+  #   render "single_product.json.jb"
+  # end
+
+  def index
+    @products = Product.all
+    render "index.json.jb"
   end
 
-  def camera_action
-    @camera = Product.find_by(id:3)
-    render "camera.json.jb"
+  def show
+    @product = Product.find(params[:id])
+    render "show.json.jb"
   end
 
-  def fan_action
-    @fan = Product.find_by(id:2)
-    render "fan.json.jb"
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_path: params[:image_path],
+      description: params[:description]
+    )
+    @product.save
+    render "show.json.jb"
   end
 
-  def laptop_action
-    @laptop = Product.find_by(id:1)
-    render "laptop.json.jb"
+  def update
+    @product = Product.find(params[:id])
+
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_path = params[:image_path] || @product.image_path
+    @product.description = params[:description] ||@product.description
+
+    @product.save
+    render "show.json.jb"
   end
 
-  def single_product_action
-    product_id = params[:id]
-    @message = Product.find_by(id: product_id)
-    render "single_product.json.jb"
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    render json: {message:"Product was destroyed"} 
   end
-
 
 end
--- front_end.rb
+++ front_end.rb
@@ -1,9 +1,33 @@
-require "http"
+# require "http"
 
-response = HTTP.get("http://localhost:3000/api/all_products_path")
-pp all_products = response.parse
+# response = HTTP.get("http://localhost:3000/api/all_products_path")
+# pp all_products = response.parse
 
-require "http"
+# require "http"
 
 # response = HTTP.get("http://localhost:3000/api/camera_path")
 # pp camera = response.parse
+
+# require "http"
+
+# response = HTTP.get("http://localhost:3000/api/products")
+
+# pp index = response.parse
+
+# require "http"
+
+# response = HTTP.create("http://localhost:3000/api/products")
+
+# pp update = response.parse
+
+# require "http"
+
+# response = HTTP.update("http://localhost:3000/api/products")
+
+# pp update = response.parse
+
+# require "http"
+
+# response = HTTP.delete("http://localhost:3000/api/products")
+
+# pp destroy = response.parse
